b6349b55e10c4dcefd1a1d839ef1300a36d69ca4
James Tippett <james.tippett@gmail.com>
2026-06-24 16:50:08 +0700

    Add precompiled NIF checksums for v0.1.0

--- checksum-Elixir.ExPdfium.Native.exs
+++ checksum-Elixir.ExPdfium.Native.exs
@@ -1,1 +1,6 @@
-%{}
+%{
+  "libex_pdfium-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:ba9c0b00165840642aaf1bd88d1670b107e49ea9fd313060a215d4fc023d1ab4",
+  "libex_pdfium-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9013d153d4db601924c7fb1622405ac11d6d4d3788cb574cc6a2d184dd989ba4",
+  "libex_pdfium-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:b0a197eb30672242a24bd6ea002cea4571df671895580657fb3b2d0439c65dbe",
+  "libex_pdfium-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:22dddf4bcadf7fd849153cf1c5d2569b5b0244202877dc7f049a315ab3cbaf48",
+}
